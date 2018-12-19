d2061845aef13b2a2d7c30255b3000f6783d8228
Kamieshi <rusack.dmitr.work@gmail.com>
2018-12-20 01:33:34 +0300

    Issues #4

--- venv/Ex/Book_1_3.py
+++ venv/Ex/Book_1_3.py
@@ -6,7 +6,12 @@ verb = ["sang", "ran", "jumped", "going", "frendly", "kill"]
 adverb = ["loudly", "quietly", "well", "badly", "many"]
 
 def random_string():
-    for item in range(5):
+    try:
+        count_item=int(input("Количество генерируемых строк: "))
+    except Exception:
+        count_item=5
+
+    for item in range(count_item):
         line_out = ""
         case = random.randint(0, 1)
         if case == 0:
@@ -16,3 +21,5 @@ def random_string():
         elif case == 1:
             line_out += random.choice(article) + " " + random.choice(substantive) + " " + random.choice(verb)
             print(line_out)
+
+random_string()
