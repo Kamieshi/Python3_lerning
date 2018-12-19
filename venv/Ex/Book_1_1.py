9508d4835e92cc82456f1b20f74c5116acc6ef03
Dzmitry <rusack.dmitr.work@gmail.com>
2018-12-19 23:38:48 +0300

    Issues #2

---
+++ venv/Ex/Book_1_1.py
@@ -0,0 +1,91 @@
+zero = ["  ****  ",
+        " **  ** ",
+        "**    **",
+        " **  ** ",
+        "  ****  "]
+
+one = ["   **   ",
+       "   **   ",
+       "   **   ",
+       "   **   ",
+       "   **   "]
+
+two = ["  ***** ",
+       " **  ** ",
+       "    **  ",
+       "  **    ",
+       " ****** "]
+
+three = ["  ***** ",
+         "     ** ",
+         "   **   ",
+         "     ** ",
+         "  ***** "]
+
+fore = [" **  ** ",
+        " **  ** ",
+        " ****** ",
+        "     ** ",
+        "     ** "]
+
+five = [" ****** ",
+        " **     ",
+        " ****** ",
+        "     ** ",
+        " ****** "]
+
+six = [" ****** ",
+       " **     ",
+       " ****** ",
+       " **  ** ",
+       " ****** "]
+
+seven = [" ****** ",
+         "     ** ",
+         "   **   ",
+         "  **    ",
+         " **     "]
+
+eight = [" ****** ",
+         " **  ** ",
+         " ****** ",
+         " **  ** ",
+         " ****** "]
+
+nine = [" ****** ",
+        " **  ** ",
+        " ****** ",
+        "     ** ",
+        " ****** "]
+point = ["        ",
+         "        ",
+         "        ",
+         "   **   ",
+         "   **   "]
+
+numbers_list = [zero, one, two, three, fore, five, six, seven, eight, nine,point]
+
+
+def convert_print(number):
+    try:
+        float(number)
+    except Exception as e:
+        print("Введите число типа float(12.5)")
+        return e
+    number = str(number)
+    line_in = ""
+    index_line = 0
+    while index_line < len(numbers_list[0]):
+        for item in number:
+            try:
+                line_in += numbers_list[int(item)][index_line]
+            except ValueError:
+                line_in += numbers_list[10][index_line]
+        index_line += 1
+        print(line_in)
+        line_in = ""
+
+    return 0
+
+
+convert_print()
