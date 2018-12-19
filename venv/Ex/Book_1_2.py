651793f399829f1b1fe9c7f86d7441cb17882dad
Kamieshi <rusack.dmitr.work@gmail.com>
2018-12-20 02:55:30 +0300

    Issues #4

--- venv/Ex/Book_1_2.py
+++ venv/Ex/Book_1_2.py
@@ -1,5 +1,23 @@
 from functools import reduce
 
+
+def sort(list_in):
+    list_out = list_in
+    index = 0
+    chek = 0
+    while True:
+        if list_out[index] > list_out[index + 1]:
+            list_out[index], list_out[index + 1] = list_out[index + 1], list_in[index]
+            chek = 1
+        index += 1
+        if index == len(list_out) - 1 and chek == 0:
+            break
+        elif index == len(list_out) - 1 and chek == 1:
+            index = 0
+            chek = 0
+    return list_out
+
+
 def calc_max():
     number_list = []
     while True:
@@ -14,7 +32,12 @@ def calc_max():
 
     print("Числа:", number_list, "\nКоличество:", len(number_list), "\nСумма:",
           reduce(lambda x, y: x + y, number_list, 0))
-    number_list.sort()
+    number_list = sort(number_list)
+    if len(number_list) % 2 == 0:
+        mediana = (number_list[int(len(number_list) / 2) - 1] + number_list[int(len(number_list) / 2)]) / 2
+    else:
+        mediana = number_list[int(len(number_list) / 2)]
     print("Наименшее:", number_list[0])
     print("Набольшее:", number_list[len(number_list) - 1])
     print("\nСреднее", reduce(lambda x, y: x + y, number_list, 0) / len(number_list))
+    print("Медиана", mediana)
